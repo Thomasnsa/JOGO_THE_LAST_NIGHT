7432da702bd295d0c2413990d0b9f54531620557
Thomasnsa <thomasnsa935@gmail.com>
2025-11-23 21:10:23 -0300

    RW

--- jogo/src/main/java/com/jogo/Main.java
+++ jogo/src/main/java/com/jogo/Main.java
@@ -2,6 +2,6 @@ package com.jogo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlddd!");
+        System.out.println("HDFSello world!");
     }
 }
